185c3f3a389698f42473cda0ad6f4be836968c24
Jason Antman <jason@jasonantman.com>
2015-09-27 21:16:31 -0400

    project skeleton

--- README.rst
+++ README.rst
@@ -1,4 +1,6 @@
-# rebuildbot
+rebuildbot
+----------
+
 Rebuildbot re-runs builds of your inactive projects.
 
 Dependencies change. Libraries break. For personal projects with slow development cycles, it might be weeks or months before a broken build or bug report uncovers the problems. ReBuildBot is a Python script that runs via cron, triggers rebuilds of your projects on TravisCI, and optionally executes some commands in a fresh git clone of master (i.e. integration or acceptance tests). You'll receive an email with the details of the runs and links to local command output uploaded to S3.
---
+++ setup.py
@@ -0,0 +1,88 @@
+"""
+setup.py
+
+The latest version of this package is available at:
+<https://github.com/jantman/rebuildbot>
+
+##################################################################################
+Copyright 2015 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+
+    This file is part of rebuildbot.
+
+    rebuildbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    rebuildbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with rebuildbot.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+##################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/rebuildbot> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+##################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+##################################################################################
+"""
+
+from setuptools import setup, find_packages
+from sys import version_info
+from rebuildbot.version import _VERSION, _PROJECT_URL
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+requires = [
+]
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Software Development :: Testing',
+]
+
+setup(
+    name='rebuildbot',
+    version=_VERSION,
+    author='Jason Antman',
+    author_email='jason@jasonantman.com',
+    packages=find_packages(),
+    #entry_points="""
+    #[console_scripts]
+    #rebuildbot = rebuildbot.runner:console_entry_point
+    #""",
+    url=_PROJECT_URL,
+    description='Rebuildbot re-runs builds of your inactive projects.',
+    long_description=long_description,
+    install_requires=requires,
+    keywords="travis travisci ci build rebuild testing",
+    classifiers=classifiers
+)
